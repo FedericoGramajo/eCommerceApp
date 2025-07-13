6971edd7751b30725052eafd0976d596d9d59ccf
FedericoGramajo <54726454+FedericoGramajo@users.noreply.github.com>
2025-07-12 22:55:34 -0300

    Identity authentication

--- eCommerceApp.Infrastructure/Data/AppDbContext.cs
+++ eCommerceApp.Infrastructure/Data/AppDbContext.cs
@@ -1,8 +1,10 @@
 ﻿using eCommerceApp.Domain.Entities;
+using eCommerceApp.Domain.Entities.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 namespace eCommerceApp.Infrastructure.Data
 {
-    public class AppDbContext : DbContext
+    public class AppDbContext : IdentityDbContext<AppUser>
     {
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
         {
@@ -10,5 +12,6 @@ namespace eCommerceApp.Infrastructure.Data
 
         public DbSet<Product> Products{ get; set; }
         public DbSet<Category> Categories { get; set; }
+        public DbSet<RefreshToken> RefreshToken { get; set; }
     }
 }
