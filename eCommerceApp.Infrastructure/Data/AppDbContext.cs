b72a8edac9848df4222fe2472ecebca72e4f217b
FedericoGramajo <54726454+FedericoGramajo@users.noreply.github.com>
2025-07-15 21:57:42 -0300

    Admin and user roles

--- eCommerceApp.Infrastructure/Data/AppDbContext.cs
+++ eCommerceApp.Infrastructure/Data/AppDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using eCommerceApp.Domain.Entities;
 using eCommerceApp.Domain.Entities.Identity;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 namespace eCommerceApp.Infrastructure.Data
@@ -13,5 +14,26 @@ namespace eCommerceApp.Infrastructure.Data
         public DbSet<Product> Products{ get; set; }
         public DbSet<Category> Categories { get; set; }
         public DbSet<RefreshToken> RefreshToken { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            
+            builder.Entity<IdentityRole>()
+                .HasData(
+                    new IdentityRole
+                    {
+                        Id = Guid.Parse("9e4f49fe-0786-44c6-9061-53d2aa84fab3").ToString(),
+                        Name = "Admin",
+                        NormalizedName = "ADMIN"
+                    },
+                    new IdentityRole
+                    {
+                        Id = Guid.Parse("3a9d0c3b-4e59-4b5e-a2b7-45c7d07f91d4").ToString(),
+                        Name = "User",
+                        NormalizedName = "USER"
+                    }
+                );
+        }
     }
 }
